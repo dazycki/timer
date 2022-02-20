c6b1760eb7a10a8544125d259b706c4c60f0f34a
Daryan Zycki <da.zycki@gmail.com>
2022-02-20 08:13:30 +0000

    Refactor to include alert when timer goes off.

--- timer1.js
+++ timer1.js
@@ -1,8 +1,8 @@
-const args = process.argv.slice(2).filter(x => x > 0).sort((a, b) => a - b); //take in argument from command line
-const delArr = args.map(x => x * 1000);
-console.log(delArr);
-for (k = 0; k < delArr.length; k++) {
+const args = process.argv.slice(2).filter(x => x > 0).sort((a, b) => a - b); //take in argument from command line and sort in ascending order
+const delArr = args.map(x => x * 1000); //multiply each value by 1000ms to convert delay into seconds
+for (let index = 0; index < delArr.length; index++) { //loops through array and alerts for each timer
   setTimeout(() => {
     process.stdout.write('\x07');
-  }, delArr[k])
+    console.log(`The ${delArr[index] / 1000}s timer has gone off!`); //prints what times was set off
+  }, delArr[index]);
 }
