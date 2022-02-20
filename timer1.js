af964bed4b089dcecc22c2a913be58412b5f047d
Daryan Zycki <da.zycki@gmail.com>
2022-02-20 00:51:20 +0000

    Refactored to filter out -ve input(s).

--- timer1.js
+++ timer1.js
@@ -1,5 +1,6 @@
-const args = process.argv.slice(2).sort((a, b) => a - b); //take in argument from command line
+const args = process.argv.slice(2).filter(x => x > 0).sort((a, b) => a - b); //take in argument from command line
 const delArr = args.map(x => x * 1000);
+console.log(delArr);
 for (k = 0; k < delArr.length; k++) {
   setTimeout(() => {
     process.stdout.write('\x07');
